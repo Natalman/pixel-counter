5b94848b6138360a06f2a68a0398b39ef194a358
TheKingNat <sperobb@outlook.com>
2016-10-18 20:08:11 -0500

    Gui design done

--- pixel/Form1.Designer.cs
+++ pixel/Form1.Designer.cs
@@ -28,12 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.PixelsLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // PixelsLabel
+            // 
+            this.PixelsLabel.AutoSize = true;
+            this.PixelsLabel.Location = new System.Drawing.Point(13, 13);
+            this.PixelsLabel.Name = "PixelsLabel";
+            this.PixelsLabel.Size = new System.Drawing.Size(88, 13);
+            this.PixelsLabel.TabIndex = 0;
+            this.PixelsLabel.Text = "Pixel in Window=";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.PixelsLabel);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label PixelsLabel;
     }
 }
 
